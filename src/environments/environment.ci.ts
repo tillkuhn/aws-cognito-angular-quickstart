7d418e326e484f97dfb36378958f95d077f8b139
tillkuhn <till.kuhn@dbschenker.com>
2018-09-06 12:12:53 +0200

    add environment for ci build

---
+++ src/environments/environment.ci.ts
@@ -0,0 +1,18 @@
+export const environment = {
+    production: false,
+    region: 'eu-central-1',
+    identityPoolId: 'test',
+    userPoolId: 'test',
+    clientId: 'test',
+    rekognitionBucket: 'test',
+    albumName: "test",
+    bucketRegion: 'eu-central-1',
+    ddbTableName: 'test',
+    cognito_idp_endpoint: 'http://localhost:4590',
+    cognito_identity_endpoint: 'http://localhost:4591',
+    sts_endpoint: 'http://localhost:4592',
+    dynamodb_endpoint: 'http://localhost:4569',
+    s3_endpoint: 'http://localhost:4572'
+
+};
+
