0070019100fd346b3fc50704bc523e827dbd30a1
tillkuhn <till.kuhn@dbschenker.com>
2018-09-27 21:04:18 +0200

    sort countries by name

--- src/app/secure/dish-detail/dish-detail.component.ts
+++ src/app/secure/dish-detail/dish-detail.component.ts
@@ -71,6 +71,8 @@ export class DishDetailComponent implements OnInit,LoggedInCallback {
             }
         });
         this.getTags();
+        // sort countries by name
+        this.origins.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
     }
 
     getTags(): void {
