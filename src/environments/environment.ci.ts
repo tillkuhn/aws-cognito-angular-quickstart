41e9ec1714e858adb7b55b4a9a93e8bc2c1fb96f
tillkuhn <till.kuhn@dbschenker.com>
2018-09-28 23:00:26 +0200

    fix (again) travis environment

--- src/environments/environment.ci.ts
+++ src/environments/environment.ci.ts
@@ -1,19 +1,23 @@
 export const environment = {
     production: false,
-    region: 'eu-central-1',
-    identityPoolId: 'test',
-    userPoolId: 'test',
-    clientId: 'test',
-    rekognitionBucket: 'test',
-    albumName: "test",
-    bucketRegion: 'eu-central-1',
-    ddbTableName: 'test',
-    cognito_idp_endpoint: 'http://localhost:4590',
-    cognito_identity_endpoint: 'http://localhost:4591',
-    sts_endpoint: 'http://localhost:4592',
-    dynamodb_endpoint: 'http://localhost:4569',
-    s3_endpoint: 'http://localhost:4572',
-    mapboxAccessToken: 'dummy'
 
+    region: 'eu-central-1',
+
+    identityPoolId: '',
+    userPoolId: '',
+    clientId: '',
+
+    bucketRegion: 'eu-central-1',
+    bucketNamePrefix: '',
+
+    ddbTableName: '',
+    ddbTableNamePrefix: '',
+    mapboxAccessToken: '',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
 };
 
