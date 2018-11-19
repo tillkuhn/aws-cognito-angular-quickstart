1affd19bf851a4398f4404d21ab2c5af4ae515ff
tillkuhn <till.kuhn@dbschenker.com>
2018-11-19 18:04:57 +0100

    add delete region, switch to terraform-aws-api-gateway-enable-cors module

--- terraform-apigw.tf
+++ terraform-apigw.tf
@@ -17,7 +17,9 @@ variable "api_gateway_stage_name" { default = "v1" }
 ## https://docs.aws.amazon.com/apigateway/latest/developerguide/apigateway-enable-cognito-user-pool.html
 #####################################################################
 
+#####################################################################
 # Create API Gateway
+#####################################################################
 resource "aws_api_gateway_rest_api" "main" {
   name        = "${var.app_id}-api"
   description = "Managed by Terraform"
@@ -26,7 +28,9 @@ resource "aws_api_gateway_rest_api" "main" {
   }
 }
 
+#####################################################################
 # Create IAM role for API Gateway
+#####################################################################
 resource "aws_iam_role" "api-gateway" {
   name = "${var.role_name_prefix}-api-gateway"
   description = "Managed by Terraform"
@@ -47,7 +51,9 @@ resource "aws_iam_role" "api-gateway" {
 EOF
 }
 
+#####################################################################
 # Create POLICY for API Gateway to access DDB table(s)
+#####################################################################
 resource "aws_iam_role_policy" "api-gateway" {
   name = "DDBPolicy"
   role = "${aws_iam_role.api-gateway.id}"
@@ -75,8 +81,10 @@ resource "aws_iam_role_policy" "api-gateway" {
 EOF
 }
 
+#####################################################################
 ## Create Authrizer to secure requests
 ## Example for usage with cognito: https://www.terraform.io/docs/providers/aws/r/api_gateway_method.html
+#####################################################################
 resource "aws_api_gateway_authorizer" "main" {
   depends_on = ["aws_cognito_user_pool.main"]
   name          = "${var.app_id}-api-authorizer"
@@ -85,79 +93,28 @@ resource "aws_api_gateway_authorizer" "main" {
   provider_arns = ["${aws_cognito_user_pool.main.arn}"]
 }
 
-# Create resouce regions (/regsions)
+#####################################################################
+# Create resoucre "regions" (/regions)
+#####################################################################
 resource "aws_api_gateway_resource" "regions" {
   rest_api_id = "${aws_api_gateway_rest_api.main.id}"
   parent_id = "${aws_api_gateway_rest_api.main.root_resource_id}"
   path_part = "regions"
 }
 
-## WELCOME TOP CORS HELL
-## Thank You: https://medium.com/@MrPonath/terraform-and-aws-api-gateway-a137ee48a8ac
-resource "aws_api_gateway_method" "options_method" {
-  rest_api_id = "${aws_api_gateway_rest_api.main.id}"
-  resource_id = "${aws_api_gateway_resource.regions.id}"
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
-resource "aws_api_gateway_method_response" "options_200" {
-  depends_on = ["aws_api_gateway_method.options_method"]
-  rest_api_id = "${aws_api_gateway_rest_api.main.id}"
-  resource_id = "${aws_api_gateway_resource.regions.id}"
-  http_method   = "${aws_api_gateway_method.options_method.http_method}"
-  status_code   = "200"
-  response_models {
-    "application/json" = "Empty"
-  }
-  response_parameters {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-}
-resource "aws_api_gateway_integration" "options_integration" {
-  depends_on = ["aws_api_gateway_method.options_method"]
-  rest_api_id = "${aws_api_gateway_rest_api.main.id}"
-  resource_id = "${aws_api_gateway_resource.regions.id}"
-  http_method   = "${aws_api_gateway_method.options_method.http_method}"
-  type          = "MOCK"
-  ## required or you get: Execution failed due to configuration error:
-  ## ... statusCode should be an integer which defined in request template
-  ## see also https://forums.aws.amazon.com/thread.jspa?threadID=248513
-  request_templates {
-    "application/json" = <<EOF
-{"statusCode": 200}
-EOF
-  }
-}
 
-resource "aws_api_gateway_integration_response" "options_integration_response" {
-  rest_api_id = "${aws_api_gateway_rest_api.main.id}"
-  resource_id = "${aws_api_gateway_resource.regions.id}"
-  http_method   = "${aws_api_gateway_method.options_method.http_method}"
-  status_code   = "${aws_api_gateway_method_response.options_200.status_code}"
-  response_parameters = {
-  #  "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,PUT'",
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-  }
-  depends_on = ["aws_api_gateway_method_response.options_200"]
-}
-## END OPTIONS
-
-
+#####################################################################
 ## Method: PUT a region
+#####################################################################
 resource "aws_api_gateway_method" "put-region" {
   rest_api_id = "${aws_api_gateway_rest_api.main.id}"
   resource_id = "${aws_api_gateway_resource.regions.id}"
   http_method = "PUT"
   authorization = "COGNITO_USER_POOLS"
   authorizer_id = "${aws_api_gateway_authorizer.main.id}"
-
 }
 
-## Method: PUT a region Request Integration
+## Method: PUT a region - Request Integration
 resource "aws_api_gateway_integration" "put-region-integration" {
   rest_api_id = "${aws_api_gateway_rest_api.main.id}"
   resource_id = "${aws_api_gateway_resource.regions.id}"
@@ -196,8 +153,7 @@ EOF
   }
 }
 
-##############
-## Method: PUT a region Response Integration
+## Method: PUT a region - Response Integration
 resource "aws_api_gateway_method_response" "put-region-response-200" {
   rest_api_id = "${aws_api_gateway_rest_api.main.id}"
   resource_id = "${aws_api_gateway_resource.regions.id}"
@@ -218,8 +174,9 @@ resource "aws_api_gateway_integration_response" "put-region-response" {
     "method.response.header.Access-Control-Allow-Origin" = "'*'"
   }
 }
-
+#####################################################################
 ## Method: GET All regions
+#####################################################################
 resource "aws_api_gateway_method" "get-region" {
   rest_api_id = "${aws_api_gateway_rest_api.main.id}"
   resource_id = "${aws_api_gateway_resource.regions.id}"
@@ -229,8 +186,7 @@ resource "aws_api_gateway_method" "get-region" {
   authorizer_id = "${aws_api_gateway_authorizer.main.id}"
 }
 
-###
-## Method: GET All regions response integration
+## Method: GET All regions - integration
 resource "aws_api_gateway_integration" "get-region-integration" {
   rest_api_id = "${aws_api_gateway_rest_api.main.id}"
   resource_id = "${aws_api_gateway_resource.regions.id}"
@@ -248,8 +204,7 @@ EOF
   }
 }
 
-###########
-## Method: GET All regions 200 response
+## Method: GET All regions - method response 200
 resource "aws_api_gateway_method_response" "get-region-response-200" {
   rest_api_id = "${aws_api_gateway_rest_api.main.id}"
   resource_id = "${aws_api_gateway_resource.regions.id}"
@@ -263,6 +218,7 @@ resource "aws_api_gateway_method_response" "get-region-response-200" {
 ## Mapping reference: https://docs.aws.amazon.com/de_de/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
 ## conditional? https://stackoverflow.com/questions/32511087/aws-api-gateway-how-do-i-make-querystring-parameters-optional-in-mapping-templa
 
+## Method: GET All regions - method response integration
 resource "aws_api_gateway_integration_response" "get-region-response" {
   depends_on = ["aws_api_gateway_integration.get-region-integration"]
   rest_api_id = "${aws_api_gateway_rest_api.main.id}"
@@ -297,7 +253,9 @@ EOF
   }
 }
 
+#####################################################################
 ## Method: DELETE REGION
+#####################################################################
 resource "aws_api_gateway_method" "delete-region" {
   rest_api_id = "${aws_api_gateway_rest_api.main.id}"
   resource_id = "${aws_api_gateway_resource.regions.id}"
@@ -307,6 +265,41 @@ resource "aws_api_gateway_method" "delete-region" {
 }
 
 
+## Method: DELETE region - integration
+resource "aws_api_gateway_integration" "delete-region-integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.main.id}"
+  resource_id = "${aws_api_gateway_resource.regions.id}"
+  http_method = "${aws_api_gateway_method.delete-region.http_method}"
+  type = "AWS"
+  integration_http_method = "POST"
+  uri = "arn:aws:apigateway:${var.aws_region}:dynamodb:action/DeleteItem"
+  credentials = "${aws_iam_role.api-gateway.arn}"
+  request_templates {
+    "application/json" = <<EOF
+{
+    "TableName": "${aws_dynamodb_table.region.name}",
+    "Key": {
+        "code": {
+            "S": "$input.path('$.code')"
+        }
+    }
+}
+EOF
+  }
+}
+
+## Method: DELETE regions - method response 200
+resource "aws_api_gateway_method_response" "delete-region-response-200" {
+  rest_api_id = "${aws_api_gateway_rest_api.main.id}"
+  resource_id = "${aws_api_gateway_resource.regions.id}"
+  http_method = "${aws_api_gateway_method.delete-region.http_method}"
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+############################
 ## And easier way to apply CORS config?
 module "cors" {
   source = "github.com/squidfunk/terraform-aws-api-gateway-enable-cors"
@@ -322,12 +315,13 @@ module "cors" {
 
 ############################
 ## Deploy the Gateway Stage
+## it seems you have to update the variable to actually force a deployment
 resource "aws_api_gateway_deployment" "main" {
   depends_on = ["aws_api_gateway_resource.regions"]
   rest_api_id = "${aws_api_gateway_rest_api.main.id}"
   stage_name  = "${var.api_gateway_stage_name}"
   variables = {
-    "answer" = "44"
+    "answer" = "45"
   }
 }
 
